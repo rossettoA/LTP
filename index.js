834de7833aa6a2b042e63698a0a83c58db1de0d5
Ana Flávia  Flávia <101286332+rossettoA@users.noreply.github.com>
2022-03-09 19:03:13 +0000

    novo let (novo)

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ class Retangulo{
     return this.calcularArea();
   }
 }
-let quadrado= new Retangulo ("10", "10");
-let retangulo= new Retangulo ("60", "30");
+let quadrado = new Retangulo ("10", "10");
+let retangulo = new Retangulo ("60", "30");
+let novo = new Retangulo ("20", "20");
+let novo1 = new Retangulo ("40", "80");
 console.log(quadrado.mostrarAtributos(), retangulo.mostrarAtributos())
+console.log(novo.mostrarAtributos(), novo1.mostrarAtributos())
 console.log(quadrado.calcularArea(), retangulo.calcularArea())
+console.log(quadrado.area(), retangulo.area())
